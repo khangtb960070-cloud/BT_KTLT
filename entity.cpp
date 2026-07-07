5acc25dd4d6dfad764b3c220d2c5ce1e3d22a6a1
khangtb960070-cloud <khangtb960070@ut.edu.vn>
2026-07-07 15:34:49 +0700

    entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -10,13 +10,13 @@ void Entity::updatePosition(int dy) {
     m_y += dy; // Vì trục Y của Qt tăng dần từ trên xuống, cộng thêm dy nghĩa là vật thể đang rơi xuống
 }
 
-// Thằng làm Logic va chạm sẽ xài hàm này để lấy ra cái khung bảo vệ của vật thể
+// làm Logic va chạm sẽ xài hàm này để lấy ra cái khung bảo vệ của vật thể
 QRectF Entity::getBoundingBox() const {
     // Trả về một đối tượng QRectF chứa đủ 4 thông số: tọa độ góc trên-trái và kích thước rộng-cao
     return QRectF(m_x, m_y, m_width, m_height);
 }
 
-// Thằng làm giao diện đồ họa sẽ gọi hàm này để biết thực thể đang đứng ở vị trí nào mà render ảnh
+// làm giao diện đồ họa sẽ gọi hàm này để biết thực thể đang đứng ở vị trí nào mà render ảnh
 QPointF Entity::getPosition() const {
     // Trả về một điểm tọa độ chứa cặp số (x, y)
     return QPointF(m_x, m_y);
